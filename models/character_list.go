0ba700e73ddf7e212bf72a3f020b371e1e3ca5fa
toluwase <toluwasethomas1@gmail.com>
2022-02-11 19:27:16 +0100

    character struct

---
+++ models/character_list.go
@@ -0,0 +1,9 @@
+package models
+
+type CharacterList struct {
+	Name string `json:"name"`
+	Gender string `json:"gender"`
+	Height float64 `json:"height"`
+	TotalNumberOfCharacters string `json:"totalNumberOfCharacters"`
+	
+}
---
+++ models/comments.go
@@ -0,0 +1,7 @@
+package models
+
+type Comments struct {
+	Body string `json:"body"`
+	IPAddress string `json:"ip_address"`
+
+}
