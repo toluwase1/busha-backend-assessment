ffca294838087b26d303c7c0a9ccd58d10c6c71d
toluwase <toluwasethomas1@gmail.com>
2022-02-11 19:28:03 +0100

    comment struct

--- models/comments.go
+++ models/comments.go
@@ -1,7 +1,11 @@
 package models
 
+import "time"
+
 type Comments struct {
+	ID uint64 `json:"id"`
+	MovieId int `json:"movie_id"`
 	Body string `json:"body"`
 	IPAddress string `json:"ip_address"`
-
+	DateCreated time.Time `json:"date_created"`
 }
