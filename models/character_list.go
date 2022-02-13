3981be185189972db64c83b7f6b351ea75ba2f7b
toluwase <toluwasethomas1@gmail.com>
2022-02-13 19:23:17 +0100

    GetCharInformation

--- models/character_list.go
+++ models/character_list.go
@@ -15,7 +15,7 @@ type CharacterList struct {
 }
 
 
-func GetCharacterListById(c *gin.Context) (*CharacterList, error) {
+func GetCharacterListById(c *gin.Context) (*[]CharacterList, error) {
 	errList = map[string]string{}
 	movieId := c.Param("movieId")
 	url:= Url+"/films/"+movieId
@@ -39,7 +39,7 @@ func GetCharacterListById(c *gin.Context) (*CharacterList, error) {
 		return nil, err
 	}
 
-	character := CharacterList{}
+	character := []CharacterList{}
 	err = json.Unmarshal(body, &character)
 	if  err != nil {
 		errList["Bad Request"] = "Could not read the specified body from the api"
@@ -55,6 +55,48 @@ func GetCharacterListById(c *gin.Context) (*CharacterList, error) {
 		"response": character,
 	})
 	return &character, nil
+}
+
+func GetCharInformation (c *gin.Context) (CharacterList, error){
+	errList = map[string]string{}
+	path := c.GetString("path")
+	url:= Url+"/films/"+path
+	response, err := http.Get(url)
+	if err != nil {
+		errList["No_character"] = "Could not get Character from link"
+		c.JSON(http.StatusBadRequest, gin.H{
+			"status": http.StatusBadRequest,
+			"error":  errList,
+		})
+		return CharacterList{}, err
+	}
+
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		errList["No_character"] = "Could not get Character from link"
+		c.JSON(http.StatusBadRequest, gin.H{
+			"status": http.StatusBadRequest,
+			"error":  errList,
+		})
+		return CharacterList{}, err
+	}
+
+	character := CharacterList{}
+	err = json.Unmarshal(body, &character)
+	if  err != nil {
+		errList["Bad Request"] = "Could not read the specified body from the api"
+		c.JSON(http.StatusBadRequest, gin.H{
+			"status": http.StatusBadRequest,
+			"error":  errList,
+		})
+		return CharacterList{}, err
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"status":   http.StatusOK,
+		"response": character,
+	})
+	return character, nil
 }
 
 
