2e40c6aa01f07537846f37e41d1dd2a256b47983
toluwase <toluwasethomas1@gmail.com>
2022-02-13 15:19:42 +0100

    GetCharacterListById

--- models/character_list.go
+++ models/character_list.go
@@ -1,8 +1,62 @@
 package models
 
+import (
+	"encoding/json"
+	"github.com/gin-gonic/gin"
+	"io/ioutil"
+	"net/http"
+)
+
 type CharacterList struct {
 	Name string `json:"name"`
 	Gender string `json:"gender"`
 	Height float64 `json:"height"`
 	TotalNumberOfCharacters string `json:"totalNumberOfCharacters"`
 }
+
+
+func GetCharacterListById(c *gin.Context) (*CharacterList, error) {
+	errList = map[string]string{}
+	movieId := c.Param("movieId")
+	url:= Url+"/films/"+movieId
+	response, err := http.Get(url)
+	if err != nil {
+		errList["No_movie"] = "Could not find Character"
+		c.JSON(http.StatusBadRequest, gin.H{
+			"status": http.StatusBadRequest,
+			"error":  errList,
+		})
+		return nil, err
+	}
+
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		errList["No_movie"] = "Could not read the specified body from the api"
+		c.JSON(http.StatusBadRequest, gin.H{
+			"status": http.StatusBadRequest,
+			"error":  errList,
+		})
+		return nil, err
+	}
+
+	character := CharacterList{}
+	err = json.Unmarshal(body, &character)
+	if  err != nil {
+		errList["Bad Request"] = "Could not read the specified body from the api"
+		c.JSON(http.StatusBadRequest, gin.H{
+			"status": http.StatusBadRequest,
+			"error":  errList,
+		})
+		return nil, err
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"status":   http.StatusOK,
+		"response": character,
+	})
+	return &character, nil
+}
+
+
+
+
